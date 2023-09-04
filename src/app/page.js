131aa9a526e1c8b31df52a6806ef6182938c0415
rnargot <margot.murvihill@gmail.com>
2023-09-03 17:04:48 -0700

    add font

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,19 @@
 import Image from 'next/image'
-import styles from './page.css'
-import folder from 'public/folder.png'
+import './page.css'
+import folder from '/public/folder.png'
 
 export default function Home() {
   return (
     <main className='main'>
       <div className='title'>discofish</div>
+      <p>text</p>
       <div>
         <Image className='concepts-logo'
           src={folder}
           alt="folder Logo"
+          width={200}
+          height={200}
         />
-
       </div>
     </main>
   )
