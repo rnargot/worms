a55e36efeae64f65bf7d4590bc66bb15af9087e0
rnargot <margot.murvihill@gmail.com>
2023-09-03 18:29:03 -0700

    audio

---
+++ src/app/audio/page.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import './page.css'
+
+export default function audio() {
+  return (
+    <main className='main'>
+      {/* <h2>
+        <Link href="/">Back to home</Link>
+      </h2> */}
+      <div className='title'> "audio souvenir TM</div>
+      <div className='description'> a personal bank of time traveling keys </div>
+      <div className='expand'> an unprogrammed frequency is implanted in the ear canal  </div>
+      <div className='expand1'> frequency is played at random moments during a specified time interval  </div>
+      <div className='expand2'> association between memories created during said interval and audio frequency are created and strengthened  </div>
+      <div className='expand3'> audio file is added to a bank of audio souvenirs </div>
+      <div className='expand4'> files serve as a portal to time travel back to programmed  interval   </div>
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,28 @@
 import Image from 'next/image'
 import './page.css'
 import folder from './components/folder.png'
+import cd from './components/cd.png'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='main'>
       <div className='title'>discofish</div>
-      <p>text</p>
       <div>
         <Image className='concepts-logo'
           src={folder}
           alt="folder Logo"
+          width={200}
+          height={200}
+        />
+      </div>
+      <h1 className="category">
+        <Link href="/audio">concepts</Link>
+      </h1>
+      <div>
+        <Image className='audio-logo'
+          src={cd}
+          alt="cd Logo"
           width={200}
           height={200}
         />
