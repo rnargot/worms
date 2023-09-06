804f2e955eb926acdcdf71f4d1c6b740b4335c37
rnargot <margot.murvihill@gmail.com>
2023-09-06 14:17:47 -0700

    update audio

--- src/app/audio/page.js
+++ src/app/audio/page.js
@@ -9,7 +9,7 @@ export default function audio() {
       <div className='description'> a personal bank of time traveling keys </div>
       <div className='expand'> an unprogrammed frequency is implanted in the ear canal  </div>
       <div className='expand1'> frequency is played at random moments during a specified time interval  </div>
-      <div className='expand2'> association between memories created during said interval and audio frequency are created and strengthened  </div>
+      <div className='expand2'> association between interval memories and audio frequency are created</div>
       <div className='expand3'> audio file is added to a bank of audio souvenirs </div>
       <div className='expand4'> files serve as a portal to time travel back to programmed  interval   </div>
     </main>
