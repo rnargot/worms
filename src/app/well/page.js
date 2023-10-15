d967ef1e8fa9b70efe0bc449fd7f587495cc1d67
rnargot <margot.murvihill@gmail.com>
2023-10-15 14:48:27 -0700

    well

---
+++ src/app/well/page.js
@@ -0,0 +1,60 @@
+"use client";
+import Image from 'next/image'
+import Link from 'next/link';
+import './page.css'
+import close from './close.png'
+import { handleClientScriptLoad } from 'next/script';
+
+export default function well() {
+  return (
+    <main className='main'>
+        <div className='container'>
+          <div className='stanza1'>
+          <div className='poem'>
+            I run my fingers over the pebble
+          </div>
+          <div className='poem'>
+            Smooth, cool, warming between my palm
+          </div>
+          <div className='poem'>
+            The air feels different this time
+          </div>
+          </div>
+          <div className='stanza'>
+          <div className='poem'>
+            I toss it forward, calling out hello
+          </div>
+          <div className='poem'>
+            It clacks its way down the stone walls of the well, deeper and deeper until a dull thud
+          </div>
+          <div className='poem'>
+            I thought I had heard someone call back, but again, it was the echo of my own voice
+          </div>
+          </div>
+          <div className='stanza'>
+          <div className='poem'>
+            I imagine the warmth fading from the pebble, too far down for me to retrieve
+          </div>
+          </div>
+          <div className='stanza'>
+          <div className='poem'>
+            When will I learn my lesson?
+          </div>
+          </div>
+        </div>
+
+    </main>
+  );
+}
+
+// i run my fingers over the pebble
+// smooth, cool, warming between my palm 
+// the air feels different this Time
+
+// i toss it forward, calling out hello 
+// it clacks its way down the stone walls of the well, deeper and deeper until a dull thud
+// i thought i had heard someone call back, but again, it was the echo of my own voice 
+
+// i imagine the warmth fading from the pebble, too far down for me to retrieve 
+
+// when will i learn my lesson? 
